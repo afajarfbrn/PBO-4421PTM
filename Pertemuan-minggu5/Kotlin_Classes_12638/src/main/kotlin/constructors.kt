a4fd6da67a707860972f85f91d98c298e2394c3b
afajarfbrn <ahmadfajarfebrian@gmail.com>
2022-03-24 20:13:45 +0700

    Tugas 4

---
+++ Pertemuan-minggu5/Kotlin_Classes_12638/src/main/kotlin/constructors.kt
@@ -0,0 +1,9 @@
+class Cars (var brand: String, var model: String, var year: Int)
+
+fun main() {
+    val c1 = Cars ("Ford", "Mustang", 1969)
+
+    println(c1.brand)
+    println(c1.model)
+    println(c1.year)
+}
